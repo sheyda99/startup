15b3c231238ec3d259169466c586917ba3a936bf
Sheyda Sultan <sheydasultan.99@gmail.com>
2023-09-01 19:21:51 +0400

    adding otp page

--- assets/js/show-message.js
+++ assets/js/show-message.js
@@ -1,14 +1,27 @@
 var forget = document.getElementById("forget-form");
 var message = document.getElementById("message");
+var emailInput = document.getElementById("email");
 
-if (forget != null) {
+if (forget != null && emailInput != null) {
     forget.addEventListener("submit", function(e) {
         if (emailInput.value != "") {
             message.style.display = "block";
             setTimeout(function () {
-                message.style.display = "none";
-            }, 5000);
+                window.open('/otp.html', '_self');
+            }, 3000);
             e.preventDefault(); // it's not correct way
         }
+    });
+}
+
+var sentAgain = document.getElementById("sent-again");
+
+if (sentAgain != null) {
+    sentAgain.addEventListener("click", function(e) {
+            message.style.display = "block";
+            setTimeout(function () {
+                message.style.display = "none";
+            }, 5000);
+            e.preventDefault();
     });
 }
